c81df2b243e12c45c393160678bfb5c5b7a31fa3
raychani1 <rajcsanyi.ladislav.it@gmail.com>
2020-12-04 14:30:05 +0100

    Toggle Toolbar, No new Line, Clear button

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+
 import 'painter.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+bool toolbarOpen = false;
 
 class MyApp extends StatelessWidget {
   @override
@@ -21,8 +24,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Graphical Search in Time Series',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xff070d59),
+        fontFamily: 'Product Sans',
       ),
+
       home: MyHomePage(title: 'Graphical Search in Time Series'),
     );
   }
@@ -37,42 +42,144 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _offsets = <Offset>[];
+  var _offsets = <Offset>[];
+
+  void openToolbar() {
+    setState(() {
+      toolbarOpen = true;
+    });
+  }
+
+  void closeToolbar() {
+    setState(() {
+      toolbarOpen = false;
+    });
+  }
+
+  bool isContinuation(Offset last, Offset next){
+    return
+      ((((next.dx -last.dx).abs() >=0) && ((next.dx -last.dx).abs() <=20)) &&
+       (((next.dy -last.dy).abs() >=0) && ((next.dy -last.dy).abs() <=20)));
+  }
 
   @override
   Widget build(BuildContext context) {
+    bool finished = false;
+    bool drawingMode = true;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        iconTheme: IconThemeData(
+            color: Color(0xfff6f5f5)
+        ),
+        title: Text(widget.title, style: TextStyle(
+            color: Color(0xfff6f5f5)
+        )),
+        actions: toolbarOpen ? <Widget>[
+            IconButton(
+                icon: const Icon(Icons.edit),
+                tooltip: 'Edit',
+                color: drawingMode ? Color(0xffee6f57) : null,
+                onPressed: () {
+                  setState(() {
+                    _offsets = [];
+                  });
+                }),
+            IconButton(
+                icon: const Icon(const IconData(0xe900, fontFamily: 'eraser')),
+                tooltip: 'Erase',
+                onPressed: () {
+                  setState(() {
+                    drawingMode = false;
+                  });
+                }),
+            IconButton(
+                icon: const Icon(Icons.undo),
+                tooltip: 'Undo',
+                onPressed: () {
+                  setState(() {
+                    if (_offsets.length < 5) {
+                      _offsets.removeRange(0, _offsets.length);
+                    } else {
+                      _offsets.removeRange(
+                          _offsets.length - 5, _offsets.length);
+                    }
+                  });
+                }),
+            IconButton(
+                icon: const Icon(Icons.clear),
+                tooltip: 'Clear Drawing',
+                onPressed: () {
+                  setState(() {
+                    _offsets = [];
+                  });
+                }),
+            IconButton(
+                icon: const Icon(Icons.navigate_next),
+                tooltip: 'Close Toolbar',
+                splashRadius: 0.1,
+                onPressed: () {
+                  closeToolbar();
+                }),
+        ] : <Widget>[
+          IconButton(
+              icon: const Icon(Icons.navigate_before),
+              tooltip: 'Show Toolbar',
+              splashRadius: 0.1,
+              onPressed: () {
+                openToolbar();
+
+              }),
+        ],
       ),
       body: GestureDetector(
-        onPanDown: (details) {
-          setState(() {
-            _offsets.add(details.localPosition);
-          });
-        },
-        onPanUpdate: (details) {
-          if(details.localPosition.dx >= _offsets.last.dx) {
-            setState(() {
-              _offsets.add(details.localPosition);
-            });
-          }
-        },
-        onPanEnd: (details) {
-          setState(() {
-            _offsets.add(null);
-          });
-        },
-        child: Center(
-          child: CustomPaint(
-            painter: Painter(_offsets),
-            child: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
+          onPanDown: (details) {
+            if (_offsets.isNotEmpty) {
+              if (isContinuation(_offsets[_offsets.length-1],details.localPosition) ) {
+                finished = false;
+                setState(() {
+                  _offsets.add(details.localPosition);
+                });
+              }
+            } else {
+              setState(() {
+                _offsets.add(details.localPosition);
+              });
+            }
+          },
+          onPanUpdate: (details) {
+//            if(_offsets.last != null) {
+//              if (details.localPosition.dx >= _offsets.last.dx && (((details.localPosition.dy - _offsets.last.dy).abs() >0) && ((details.localPosition.dy - _offsets.last.dy).abs() <=1)) ) {
+//                setState(() {
+//                  _offsets.add(details.localPosition);
+//                });
+//              }
+//            } else {
+//              if(isContinuation(_offsets[_offsets.length-1], details.localPosition)){
+//                setState(() {
+//                  _offsets.add(details.localPosition);
+//                });
+//              }
+//            }
+            if(details.localPosition.dx >= _offsets.last.dx && isContinuation(_offsets[_offsets.length-1],details.localPosition)) { // THIS CAUSES PROBLEMS
+              setState(() {
+                _offsets.add(details.localPosition);
+              });
+            }
+          },
+          onPanEnd: (details) {
+            finished = true;
+          },
+          child: Center(
+            child: CustomPaint(
+              painter: Painter(_offsets),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+              ),
             ),
           ),
         ),
-      ),
     );
   }
 }
--- lib/painter.dart
+++ lib/painter.dart
@@ -10,23 +10,29 @@ class Painter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    final pointMode = PointMode.polygon;
     final paint = Paint()
-      ..color = Colors.blue
+      ..color = Color(0xff1f3c88)
       ..isAntiAlias = true
-      ..strokeWidth = 5.0;
+      ..strokeWidth = 7.0;
 
-    for (var i = 0; i<offsets.length;i++) {
-      if(shouldDrawLine(i)){
-        canvas.drawLine(offsets[i], offsets[i+1], paint);
-      } else if(shouldDrawPoint(i)) {
-        canvas.drawPoints(PointMode.points, [offsets[i]], paint);
-      }
+//    for (var i = 0; i<offsets.length-1;i++) {
+//      if(shouldDrawLine(i)){
+//        canvas.drawLine(offsets[i], offsets[i+1], paint);
+//      } else if(shouldDrawPoint(i)) {
+//        canvas.drawPoints(PointMode.points, [offsets[i]], paint);
+//      }
+//    }
+    if (offsets.length > 0) {
+      canvas.drawPoints(pointMode, offsets, paint);
     }
   }
 
-  bool shouldDrawPoint(int i) => offsets[i] != null && offsets.length > i + 1 && offsets[i + 1] == null;
+  bool shouldDrawPoint(int i) =>
+      offsets[i] != null && offsets.length > i + 1 && offsets[i + 1] == null;
 
-  bool shouldDrawLine(int i) => offsets[i] != null && offsets.length > i + 1 && offsets[i + 1] != null;
+  bool shouldDrawLine(int i) =>
+      offsets[i] != null && offsets.length > i + 1 && offsets[i + 1] != null;
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
